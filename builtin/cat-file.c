dbdc9e4df0f5a4b52731346051d9c963d60905e3
Toon Claes <toon@iotcl.com>
2023-01-16 20:07:49 +0100

    cat-file: quote-format name in error when using -z
    Since it's supported to have NUL-delimited input, introduced in
    db9d67f2e9 (builtin/cat-file.c: support NUL-delimited input with `-z`,
    2022-07-22), it's possible to pass paths that contain newlines. This
    works great when the object is found, but when it's not, the input path
    is returned in the error message. Because this can contain newlines, the
    error message might get spread over multiple lines, making it harder to
    machine-parse this error message.
    
    With this change, the input is quote-formatted in the error message, if
    needed. This ensures the error message is always on a single line and
    makes parsing the error more straightforward.
    
    Signed-off-by: Toon Claes <toon@iotcl.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -14,6 +14,7 @@
 #include "tree-walk.h"
 #include "oid-array.h"
 #include "packfile.h"
+#include "quote.h"
 #include "object-store.h"
 #include "promisor-remote.h"
 #include "mailmap.h"
@@ -455,8 +456,17 @@ static void batch_object_write(const char *obj_name,
 						       &data->oid, &data->info,
 						       OBJECT_INFO_LOOKUP_REPLACE);
 		if (ret < 0) {
+			struct strbuf quoted = STRBUF_INIT;
+
+			if (opt->nul_terminated &&
+			    obj_name) {
+				quote_c_style(obj_name, &quoted, NULL, 0);
+				obj_name = quoted.buf;
+			}
+
 			printf("%s missing\n",
 			       obj_name ? obj_name : oid_to_hex(&data->oid));
+			strbuf_release(&quoted);
 			fflush(stdout);
 			return;
 		}
@@ -503,6 +513,13 @@ static void batch_one_object(const char *obj_name,
 	result = get_oid_with_context(the_repository, obj_name,
 				      flags, &data->oid, &ctx);
 	if (result != FOUND) {
+		struct strbuf quoted = STRBUF_INIT;
+
+		if (opt->nul_terminated) {
+			quote_c_style(obj_name, &quoted, NULL, 0);
+			obj_name = quoted.buf;
+		}
+
 		switch (result) {
 		case MISSING_OBJECT:
 			printf("%s missing\n", obj_name);
@@ -527,6 +544,8 @@ static void batch_one_object(const char *obj_name,
 			       result);
 			break;
 		}
+
+		strbuf_release(&quoted);
 		fflush(stdout);
 		return;
 	}
